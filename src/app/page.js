c044a102b2eea879ff321928ba6b0d163ac2bd19
Baasandash0307 <bsskaa.kai123@gmail.com>
2025-02-09 21:31:36 +0800

    newchanges

--- src/app/page.js
+++ src/app/page.js
@@ -30,7 +30,7 @@ export default function Home() {
 
   function toggleCompleted(index) {
     const clonedTasks = [...tasks];
-    clonedTasks[index].completed = !clonedTasks[index].completed;
+    clonedTasks[index].completed = !clonedTasks[index].completed
     setTasks(clonedTasks);
   }
 
